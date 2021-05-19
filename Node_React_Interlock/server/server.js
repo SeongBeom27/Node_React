b7f0b3fdbf73ee8c5719d0e77fea1732be29cb8e
SeongBeom27 <lgh121546@naver.com>
2021-05-20 00:16:33 +0900

    Node React 10. Data transfer React to Express (axios 사용해보기)

--- Node_React_Interlock/server/server.js
+++ Node_React_Interlock/server/server.js
@@ -29,6 +29,24 @@ app.use('/topic', function (req, res) {
     res.json({ topics: topics })
   })
 })
+// https://stackoverflow.com/questions/51115640/how-to-send-form-data-from-react-to-express/51116082
+// https://stackoverflow.com/questions/54952355/how-to-post-data-from-react-to-express
+// 참고하기
+app.post('/create_process', function (req, res) {
+  console.log(req)
+  var post = req.body
+  db.query(
+    `INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), 1);`,
+    [post.title, post.description, post.author],
+    function (error, result) {
+      // dbquery function의 result 객체는 insertId라는 key를 가지고 있다.
+      // res.writeHead(302, { Location: `/topic/${result.insertId}` });
+      // res.end();
+      console.log(post.title, post.description, post.author)
+      res.redirect(`http://localhost:3000/`)
+    }
+  )
+})
 
 app.listen(port, () => {
   console.log(`express is running on ${port}`)
---
+++ Node_React_Interlock/src/components/Create.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import './Create.css'
+
+class Create extends Component {
+  constructor(props) {
+    super(props)
+
+    this.Gettitles = this.Gettitles.bind(this)
+  }
+  Gettitles() {
+    return this.props.topics.map(topic => (
+      <button
+        //onClick={this.titleClick.bind(this, topic.title)}
+        value={topic.id}
+        onClick={function (e) {
+          this.props.onChangePage(e.target.value)
+        }.bind(this)}
+      >
+        {topic.title}
+      </button>
+    ))
+  }
+  render() {
+    return (
+      <div className="body-container">
+        <div className="body-left">{this.Gettitles()}</div>
+        <div className="body-right">
+          <div className="body-create">
+            <form
+              action="http://localhost:3001/create_process"
+              method="post"
+              /**
+               * submit 버튼을 클릭했을 때, submit 버튼을 포함하고 있는 form 태그 내부 OnSubmit을 호출하게 된다.
+               *
+               * e.preventDefault() 함수를 쓰면 화면이 /create_process로 이동되는 것을 방지해 준다.
+               *
+               * 현재 우리는 page 전환 없이 만들려고하는 중
+               *  */
+              onSubmit={function (e) {
+                // onSubmit의 e.target property는 form data를 담고 있다.
+                // props인 _title, _desc에 아래 e.target.title.value, e.target.desc.value를 넘겨준다.
+                this.props.onSubmit(e.target.title.value, e.target.desc.value)
+              }.bind(this)}
+            >
+              <p>
+                <input type="text" name="title" placeholder="title" />
+              </p>
+              <p>
+                <textarea name="desc" placeholder="description"></textarea>
+              </p>
+              <p>
+                <input type="submit" />
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Create
